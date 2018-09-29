cc7b8ec222d594db105bbd50ac45e1e2ae88c4a6
RealySmiLe <41011070+saadxd14@users.noreply.github.com>
2018-09-29 13:23:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,39 @@
 const Discord = require('discord.js');
-const Util = require('discord.js');  
 const client = new Discord.Client();
-const prefix = '$';
+const prefix = '$'
 const dateFormat = require('dateformat');
 const Canvas = require('canvas')
 const jimp = require('jimp')
 const moment = require("moment");
 
-client.on('ready',  () => {
-  console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
-  console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
+
 
 
 client.on('message', message => {
