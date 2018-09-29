50851aa175be4701b6de179e876f918e76018384
RealySmiLe <41011070+saadxd14@users.noreply.github.com>
2018-09-29 17:14:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,7 +82,7 @@ const w = ['./welc1.png'];
              
           
 
-                var url = member.author.avatarURL;
+                let url = member.author.avatarURL;
                 jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
