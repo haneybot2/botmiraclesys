813bb6569cc07e71554045677d4480faa246de40
RealySmiLe <41011070+saadxd14@users.noreply.github.com>
2018-09-29 13:34:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
 const dateFormat = require('dateformat');
-const Canvas = require('canvas')
+const {Canvas, canvas} = require('canvas')
 const jimp = require('jimp')
 const moment = require("moment");
 
@@ -10,7 +10,7 @@ const moment = require("moment");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`Miracle`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
